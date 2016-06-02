57e427b53e1879df452b99329ddce28900751b74
Khoa Bui <khoabui@Khoas-MacBook-Pro.local>
2016-06-02 08:00:01 +0300

    remove meteor support, change to express support

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 #install MongoDB
@@ -9,10 +9,3 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-
-#install meteor
-curl https://install.meteor.com/ | sh
-
-#synced_folder
-
-rsync -r oulu-grad/ synced-folder/ 
